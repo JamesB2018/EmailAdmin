2acb855a2a218a2b126bac89b562749a4d106e8a
James <jmb47580@ucmo.edu>
2019-07-28 18:03:59 -0500

    Added more code expressing departments

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -13,5 +15,15 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + "" + this.lastName);
+
+        //Call method asking for departmen - return the department
+    }
+
+    //Ask for the department
+    private String setDepartment() {
+        System.out.println("Enter the department\n1 for sales\n2 for development\n3 for Accounting\n0 for none");
+        Scanner in = new Scanner(System.in);
+        depChoice = in.nextInt();
+        if()
     }
 }
